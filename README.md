564250fb107bb74c49841e98486d55a9ddbf5d8e
Motalleb Fallahnehzad <fmotalleb@gmail.com>
2023-11-06 12:21:37 +0330

    [Init]

---
+++ README.md
@@ -0,0 +1,9 @@
+# nu_plugin_port_scan
+
+A nushell plugin for scanning ports on a target
+
+It acts like an extension on the port command
+
+# Examples
+
+# Installing
---
+++ src/main.rs
@@ -0,0 +1,89 @@
+use std::{
+    net::{SocketAddr, TcpStream},
+    time::Duration,
+};
+
+use nu_plugin::{self, EvaluatedCall, LabeledError};
+use nu_protocol::{Category, PluginSignature, Record, Span, SyntaxShape, Value};
+
+pub struct Plugin;
+
+impl nu_plugin::Plugin for Plugin {
+    fn signature(&self) -> Vec<PluginSignature> {
+        vec![PluginSignature::build("port scan")
+            .required(
+                "target",
+                SyntaxShape::String,
+                "target address to check for open port",
+            )
+            .required("port", SyntaxShape::Int, "port to be checked")
+            .named(
+                "timeout",
+                SyntaxShape::Duration,
+                "time before giving up the connection. (default: 60 Seconds)",
+                Option::Some('t'),
+            )
+            .usage("scan port on a target")
+            .category(Category::Experimental)]
+    }
+
+    fn run(
+        &mut self,
+        _name: &str,
+        call: &EvaluatedCall,
+        _input: &Value,
+    ) -> Result<Value, LabeledError> {
+        let default_timeout = 60000000000;
+        let cols = vec!["address".to_owned(), "port".to_owned(), "result".to_owned()];
+        let target: Value = call.req(0).unwrap();
+        let port: Value = call.req(1).unwrap();
+
+        let timeout: i64 = call
+            .get_flag_value("timeout")
+            .unwrap_or(Value::duration(default_timeout, call.head))
+            .as_duration()
+            .unwrap();
+        let duration = Duration::from_nanos(timeout.unsigned_abs());
+
+        let address = format!("{}:{}", target.as_string().unwrap(), port.as_int().unwrap())
+            .parse::<SocketAddr>();
+
+        if address.is_err() {
+            let span = Span::new(target.span().start, port.span().end);
+            return Err(LabeledError {
+                label: "Parse Address Error".to_string(),
+                msg: "Check the given address".to_string(),
+                span: Option::Some(span),
+            });
+        }
+        if let Ok(_) = TcpStream::connect_timeout(&address.unwrap(), duration) {
+            return Ok(Value::Record {
+                val: Record {
+                    cols: cols,
+                    vals: vec![
+                        Value::string(target.as_string().unwrap(), target.span()),
+                        Value::string(port.as_int().unwrap().to_string(), port.span()),
+                        Value::string("Open".to_string(), call.head),
+                    ],
+                },
+                internal_span: call.head,
+            });
+        } else {
+            return Ok(Value::Record {
+                val: Record {
+                    cols: cols,
+                    vals: vec![
+                        Value::string(target.as_string().unwrap(), target.span()),
+                        Value::string(port.as_int().unwrap().to_string(), port.span()),
+                        Value::string("Closed".to_string(), call.head),
+                    ],
+                },
+                internal_span: call.head,
+            });
+        }
+    }
+}
+
+fn main() {
+    nu_plugin::serve_plugin(&mut Plugin {}, nu_plugin::MsgPackSerializer {})
+}
