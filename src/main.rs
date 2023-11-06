66ea226dfbabd118919213575bfbbcce6d083a27
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2023-11-06 15:26:59 +0330

    [Minor]

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
+use core::panic;
 use std::{
     net::{SocketAddr, TcpStream},
     time::{Duration, Instant},
 };
 
 use nu_plugin::{self, EvaluatedCall, LabeledError};
-use nu_protocol::{Category, PluginSignature, Record, Span, SyntaxShape, Value};
+use nu_protocol::{Category, PluginExample, PluginSignature, Record, Span, SyntaxShape, Value};
 
 pub struct Plugin;
 
+const DEFAULT_TIMEOUT: i64 = 60000000000;
 impl nu_plugin::Plugin for Plugin {
     fn signature(&self) -> Vec<PluginSignature> {
         vec![PluginSignature::build("port scan")
+            .usage("The `port scan` command serves a similar purpose to the `nc -vz {ip} {port}` command,\nIt allows you to detect open ports on a target and provides valuable information about the connection time.")
             .required(
-                "target",
+                "target ip",
                 SyntaxShape::String,
-                "target address to check for open port",
+                "target IP address to check for open port",
             )
             .required("port", SyntaxShape::Int, "port to be checked")
             .named(
@@ -23,8 +26,61 @@ impl nu_plugin::Plugin for Plugin {
                 "time before giving up the connection. (default: 60 Seconds)",
                 Option::Some('t'),
             )
-            .usage("scan port on a target")
-            .category(Category::Experimental)]
+            .plugin_examples(
+                vec![
+                    PluginExample{
+                        example: "port scan 8.8.8.8 53 -t 1sec".to_string(),
+                        description : "this will create a Tcp connection to port 53 on 8.8.8.8 (Google's public dns) and return the connection time".to_string(),
+                        result: Option::Some(
+                            Value::Record {
+                                val: Record {
+                                    cols: vec![
+                                        "address".to_owned(),
+                                        "port".to_owned(),
+                                        "result".to_owned(),
+                                        "elapsed (ms)".to_string(),
+                                    ],
+                                    vals: vec![
+                                        Value::string("8.8.8.8".to_string(), Span::new(0, 0)),
+                                        Value::string("53".to_string(), Span::new(0, 0)),
+                                        Value::string("Open".to_string(), Span::new(0, 0)),
+                                        Value::string("42".to_string(), Span::new(0, 0)),
+                                    ],
+                                },
+                                internal_span: Span::new(0, 0),
+                            }
+                        ),
+                    },
+                    PluginExample{
+                        example: "port scan 8.8.8.8 54 -t 1sec".to_string(),
+                        description : "this will create a Tcp connection to port 54 on 8.8.8.8 (Google's public dns). this will result in an error".to_string(),
+                        result: Option::Some(
+                            Value::Record {
+                                val: Record {
+                                    cols: vec![
+                                        "address".to_owned(),
+                                        "port".to_owned(),
+                                        "result".to_owned(),
+                                        "elapsed (ms)".to_string(),
+                                    ],
+                                    vals: vec![
+                                        Value::string("8.8.8.8".to_string(), Span::new(0, 0)),
+                                        Value::string("53".to_string(), Span::new(0, 0)),
+                                        Value::string("Closed".to_string(), Span::new(0, 0)),
+                                        Value::string("1000".to_string(), Span::new(0, 0)),
+                                    ],
+                                },
+                                internal_span: Span::new(0, 0),
+                            }
+                        ),
+                    },
+                    PluginExample{
+                        example: "7880..8000 | each { |it| port scan 127.0.0.1 $it -t 1ms } | where result == Open".to_string(),
+                        description : "This command will scan any port from 7880 to 8000 on localhost and return open ports in range".to_string(),
+                        result: Option::None,
+                    },
+                ],)
+            .category(Category::Network)]
     }
 
     fn run(
@@ -33,70 +89,95 @@ impl nu_plugin::Plugin for Plugin {
         call: &EvaluatedCall,
         _input: &Value,
     ) -> Result<Value, LabeledError> {
-        let default_timeout = 60000000000;
         let cols = vec![
             "address".to_owned(),
             "port".to_owned(),
             "result".to_owned(),
             "elapsed (ms)".to_string(),
         ];
-        let target: Value = call.req(0).unwrap();
-        let port: Value = call.req(1).unwrap();
+        let (target, port) = extract_params(call);
 
-        let timeout: i64 = call
-            .get_flag_value("timeout")
-            .unwrap_or(Value::duration(default_timeout, call.head))
-            .as_duration()
-            .unwrap();
+        let (real_target, real_port) = load_address(&target, &port);
+
+        let timeout: i64 = match call.get_flag_value("timeout") {
+            Some(duration) => match duration.as_duration() {
+                Ok(value) => value,
+                Err(_) => panic!("Error reading duration"),
+            },
+            None => DEFAULT_TIMEOUT,
+        };
         let duration = Duration::from_nanos(timeout.unsigned_abs());
 
-        let address = format!("{}:{}", target.as_string().unwrap(), port.as_int().unwrap())
-            .parse::<SocketAddr>();
+        let address = format!("{}:{}", real_target, port.as_int().unwrap()).parse::<SocketAddr>();
 
         if address.is_err() {
             let span = Span::new(target.span().start, port.span().end);
             return Err(LabeledError {
                 label: "Address parser exception".to_string(),
                 msg: format!(
-                    "as `{}:{}` got `{}` error",
-                    target.as_string().unwrap(),
-                    port.as_int().unwrap(),
+                    "as `{}:{}` got `{}` error. do not use domain name in address",
+                    real_target,
+                    real_port,
                     address.err().unwrap().to_string()
                 ),
                 span: Option::Some(span),
             });
         }
         let now = Instant::now();
-        if let Ok(_) = TcpStream::connect_timeout(&address.unwrap(), duration) {
-            let elapsed = now.elapsed().as_millis();
-            return Ok(Value::Record {
-                val: Record {
-                    cols: cols,
-                    vals: vec![
-                        Value::string(target.as_string().unwrap(), target.span()),
-                        Value::string(port.as_int().unwrap().to_string(), port.span()),
-                        Value::string("Open".to_string(), call.head),
-                        Value::string(elapsed.to_string(), call.head),
-                    ],
-                },
-                internal_span: call.head,
-            });
-        } else {
-            let elapsed = now.elapsed().as_millis();
-            return Ok(Value::Record {
-                val: Record {
-                    cols: cols,
-                    vals: vec![
-                        Value::string(target.as_string().unwrap(), target.span()),
-                        Value::string(port.as_string().unwrap(), port.span()),
-                        Value::string("Closed".to_string(), call.head),
-                        Value::string(elapsed.to_string(), call.head),
-                    ],
-                },
-                internal_span: call.head,
-            });
-        }
+        let is_open = check_connection(address, duration);
+        let elapsed = now.elapsed().as_millis();
+        let result = match is_open {
+            true => "Open",
+            false => "Closed",
+        };
+        Ok(Value::Record {
+            val: Record {
+                cols: cols,
+                vals: vec![
+                    Value::string(real_target, target.span()),
+                    Value::string(real_port.to_string(), port.span()),
+                    Value::string(result.to_string(), call.head),
+                    Value::string(elapsed.to_string(), call.head),
+                ],
+            },
+            internal_span: call.head,
+        })
     }
+}
+
+fn check_connection(
+    address: Result<SocketAddr, std::net::AddrParseError>,
+    duration: Duration,
+) -> bool {
+    match TcpStream::connect_timeout(&address.unwrap(), duration) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+fn load_address(target: &Value, port: &Value) -> (String, i64) {
+    let real_target = match target.as_string() {
+        Ok(value) => value,
+        Err(_) => panic!("Address value cannot be parsed to string"),
+    };
+
+    let real_port = match port.as_int() {
+        Ok(value) => value,
+        Err(_) => panic!("Port value cannot be parsed to integer"),
+    };
+    (real_target, real_port)
+}
+
+fn extract_params(call: &EvaluatedCall) -> (Value, Value) {
+    let target: Value = match call.req(0) {
+        Ok(value) => value,
+        Err(_) => panic!("Given value for target is not correct"),
+    };
+    let port: Value = match call.req(1) {
+        Ok(value) => value,
+        Err(_) => panic!("Given value for port is not correct"),
+    };
+    (target, port)
 }
 
 fn main() {
